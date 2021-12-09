e85993771ea5b2e7a124fb7af369399a3176420d
Richard Mitchell <richard.a.mitchell@gmail.com>
2021-12-09 10:26:17 -0600

    Create 16.py
    Solution to Pybites bite 16.

---
+++ 16.py
@@ -0,0 +1,10 @@
+# Solution to Pybites 16: https://codechalleng.es/bites/16/
+from datetime import datetime, timedelta
+
+PYBITES_BORN = datetime(year=2016, month=12, day=19)
+
+def gen_special_pybites_dates():
+    date = PYBITES_BORN
+    while True:
+        date += timedelta(days=100)
+        yield date
